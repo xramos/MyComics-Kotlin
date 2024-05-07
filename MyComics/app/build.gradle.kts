661bd69e0231f4a4e281e9326fa9ef1d9d114ab4
Xavi <xavier.ramos@wefox.com>
2024-05-07 17:44:56 +0200

    feat: Added coil library

--- MyComics/app/build.gradle.kts
+++ MyComics/app/build.gradle.kts
@@ -100,6 +100,9 @@ dependencies {
     kapt("androidx.hilt:hilt-compiler:1.2.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
+    // Coil
+    implementation("io.coil-kt:coil-compose:1.3.2")
+
     // JUnit
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- MyComics/app/src/main/java/com/xramos/mycomics/ui/component/LisItemCharacter.kt
+++ MyComics/app/src/main/java/com/xramos/mycomics/ui/component/LisItemCharacter.kt
@@ -8,14 +8,24 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
 import com.xramos.mycomics.domain.model.model.CharacterModel
 
+@OptIn(ExperimentalCoilApi::class)
 @Composable
 fun ListItemCharacter(item: CharacterModel,
                       onClick: (id: Int) -> Unit) {
+
+    val painter = rememberImagePainter(data = item.image.smallUrl) {
+        crossfade(1000)
+        //error drawable
+        //placeholder drawable
+    }
 
     Card(
         modifier = Modifier
@@ -27,40 +37,29 @@ fun ListItemCharacter(item: CharacterModel,
         elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
     ) {
         Row(
-            Modifier.background(
-                brush = Brush.horizontalGradient(
-                    colors = listOf(
-                        Color(0xFF663399),
-                        Color(0xFF4066E0),
-                    )
-                )
-            ),
-            verticalAlignment = Alignment.CenterVertically,
-
+            verticalAlignment = Alignment.CenterVertically
             ) {
 
-            // TODO: Add Coil library and then add AsyncImage
+            Image(painter = painter,
+                contentDescription = null,
+                modifier = Modifier.size(80.dp, 80.dp).padding(8.dp),
+                contentScale = ContentScale.Fit)
 
             Column(Modifier.padding(8.dp)) {
                 Text(
                     text = item.name,
                     fontSize = 24.sp,
                     style = TextStyle(
-                        color = Color.White,
+                        color = Color.Black,
                     ),
                 )
                 Row {
-                    Text(
-                        text = "Real Name ",
-                        style = TextStyle(
-                            color = Color.White,
-                        ),
-                    )
                     item.realName?.let {
                         Text(
                             text = it,
+                            fontSize = 16.sp,
                             style = TextStyle(
-                                color = Color.White,
+                                color = Color.Black,
                             ),
                         )
                     }
